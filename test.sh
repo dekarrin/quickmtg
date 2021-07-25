68ba9ddfaf6e8e042360868b05dbdf8db3307add
Rebecca Nelson <dekarrin.irl@gmail.com>
2021-07-25 12:42:21 -0500

    Update test file

--- test.sh
+++ test.sh
@@ -3,8 +3,11 @@
 # need to execute everything from this directory
 cd "$(dirname "$0")"
 
+set -x
 echo "Placing all output from tests in test_output/ directory..."
 mkdir test_output 2>/dev/null
 
-echo "Attempting to generate binder view..."
+echo "Attempting to generate binder views..."
+./qmtg view create testfiles/one_inventory.txt test_output/one_binder || exit $ERR_TEST
 ./qmtg view create testfiles/small_inventory.txt test_output/small_binder || exit $ERR_TEST
+./qmtg view create testfiles/medium_inventory.txt test_output/medium_binder || exit $ERR_TEST
