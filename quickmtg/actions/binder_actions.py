7479f6cb843feb4b70caf42ba16d9fadebabb0d4
Becky Nelson <dekarrin.irl@gmail.com>
2021-07-30 07:38:44 -0500

    remove debugging statments

--- quickmtg/actions/binder_actions.py
+++ quickmtg/actions/binder_actions.py
@@ -69,13 +69,8 @@ def create(store: storage.AutoSaveObjectStore, api: scryfall.ScryfallAgent, list
         owned.condition = c.condition
         owned.count = c.count
         cards.append(owned)
-
-    import pprint
-    _log.debug("PRE: ")
-    _log.debug(pprint.pformat(cards))
+    
     cards = sorted(cards)
-    _log.debug("POST: ")
-    _log.debug(pprint.pformat(cards))
 
     # cards are now gotten, generate html:
     _log.info("(3/6) Generating binder pages...")
